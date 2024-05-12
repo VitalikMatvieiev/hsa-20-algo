eb0ac2b96505de3565a8a384ac0b2935484db5fa
Vitalii Matvieiev <vitalik.matveev133@gmail.com>
2024-05-12 10:11:08 -0500

    Init

---
+++ BBST_and_CountingSort/Program.cs
@@ -0,0 +1,17 @@
+﻿BinaryTree tree = new BinaryTree();
+tree.root = new Node(10);
+tree.root.left = new Node(8);
+tree.root.left.left = new Node(7);
+tree.root.left.left.left = new Node(6);
+tree.root.left.left.left.left = new Node(5);
+
+tree.root = tree.buildTree(tree.root);
+
+List<int> inputArray = new List<int> { 4, 3, 12, 1, 5, 5, 3, 9 };
+// Output array
+List<int> outputArray = CountingSort.CountSort(inputArray);
+for (int i = 0; i < inputArray.Count; i++)
+    Console.Write(outputArray[i] + " ");
+Console.WriteLine();
+
+var x = 1;
